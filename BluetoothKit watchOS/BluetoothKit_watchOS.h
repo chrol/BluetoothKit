1ffb45021dc3c5ed1196ef8c3261b63ed28c3a0f
Christian Olrog <christian@olrog.se>
2020-10-25 22:12:23 +0100

    Merging watchos to master

---
+++ BluetoothKit watchOS/BluetoothKit_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  BluetoothKit_watchOS.h
+//  BluetoothKit watchOS
+//
+//  Created by Christian Olrog on 2019-04-27.
+//  Copyright © 2019 Rasmus Taulborg Hummelmose. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BluetoothKit_watchOS.
+FOUNDATION_EXPORT double BluetoothKit_watchOSVersionNumber;
+
+//! Project version string for BluetoothKit_watchOS.
+FOUNDATION_EXPORT const unsigned char BluetoothKit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothKit_watchOS/PublicHeader.h>
+
+
